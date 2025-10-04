e7e14be9d91c148e1640b730818d6c1f8659b4fb
foksi <foksiigoldi@gmail.com>
2025-10-04 11:07:30 +0900

    Переработка системы player и сохранение персонажа

--- main.py
+++ main.py
@@ -1,35 +1,21 @@
 try:
     import random
     from item import *
+    from mobs import *
+    import json
 except:
-    print("всё норм")
-
-class Player:
-    def __init__(self):
-        self.name = None
-        self.age = None
-        self.health = 100
-        self.stamina = 100
-        self.inventory = []
-    
-    def create_player(self):
-        self.name = str(input("Введите имя персонажа: "))
-        self.age = random.randint(18,40)
-    
-    def __str__(self):
-        return f"""\nВы успешно создали персонажа!\nИмя: {self.name},\nВозраст: {self.age}, \nЗдоровье: {self.health}, \nСтамина: {self.stamina}, \nИнвентарь: {self.inventory}"""  
-Player1 = Player()
-
+    print("всё не норм")
 
 def Menu():
     while True:
         try:
             print("Меню")
-            Menu_input = int(input("""Используйте ввод цифр что-бы управлять\n1. Начать новую игру\n2. Продолжить\n"""))
+            Menu_input = int(input("""Используйте ввод цифр что-бы управлять\n1. Начать новую игру\n2. Продолжить\nВвод: """))
             if Menu_input == 1:
+                print("")
                 print("Создайте своего персонажа")
-                Player1.create_player()
-                print(Player1)
+                save_player()
+                print("")
             elif():
                 print("Вы ввели неправильное значение\n")
         except:
--- mobs.py
+++ mobs.py
@@ -1,16 +1,27 @@
 import random
-# def create_player():
-class Player:
-    def __init__(self):
-        self.name = None
-        self.age = None
-        self.health = 100
-        self.stamina = 100
-        self.inventory = []
-    
-    def create_player(self):
-        self.name = input("Введите имя персонажа: ")
-        self.age = random.randint(18,40)
-    
-    def __str__(self):
-        return f"""\nВы успешно создали персонажа!\nИмя: {self.name},\nВозраст: {self.age}, \nЗдоровье: {self.health}, \nСтамина: {self.stamina}, \nИнвентарь: {self.inventory}"""
+import json
+def Igrok():
+    Player_create = {
+        "Name": None,
+        "Age": None,
+        "HP": 100,
+        "Stamina": 100,
+        "LVL": 1
+    }
+    return Player_create
+
+def create_player():
+    player = Igrok()
+    player["Name"] = input("Введите имя персонажа: ")
+    player["Age"] = random.randint(18, 40)
+    print(f"\nПерсонаж создан: \nИмя: {player['Name']}\nВозраст: {player['Age']} лет\nВаш уровень: {player['LVL']}")
+    return player
+
+def save_player():
+    with open('D:\python_cod\game\saves\save_player.json', 'w', encoding='utf-8') as file:
+        json.dump(create_player(), file, ensure_ascii=False, indent=4)
+
+def look_player():
+    with open('D:\python_cod\game\saves\save_player.json', 'r', encoding='utf-8') as file:
+        data = json.load(file)
+        print(data)
